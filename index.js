108358183c49acf3c2f8622dcec846d7c1ec2aec
Adejonwo Mubaraq Taiwo <adejonwomubaraqtaiwo@gmail.com>
2023-07-25 12:44:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function generateSentence(desc, arr) {
+    let output = `The`
+    for(let i = 0; i < arr.length; i++) {
+    return desc + " are " + arr[i]
+    }
+    
+    }
+    generateSentence(desc, arr)
